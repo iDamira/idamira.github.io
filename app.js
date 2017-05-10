987234eb764ffb433455338e5ded2dda40e5c5df
Damira Ibragimova <idamira@Damiras-MBP.home>
2017-05-09 21:52:20 -0400

    added basic logo animations

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
 $(document).ready(function(){
   console.log("js connected");
 
+  var bracketLeft = $("#bracket-left");
+  var bracketRight = $("#bracket-right");
 
+  var rotate = function(){
+    bracketLeft.css({
+      'transform': 'rotate(-360deg)'
+    }, 1000).animate({
+      opacity: 1
+    }, 400);
+        bracketRight.css({
+      'transform': 'rotate(360deg)',
+    }, 1000).animate({
+      opacity: 1
+    }, 400);
+  };
+
+  var moveToPositions = function(){
+    bracketLeft.animate({
+      left: '40vw'
+    }, 400);
+    bracketRight.animate({
+      right: '40vw'
+    }, 400);
+  }
+
+  var textAppear = function(){
+    $("h1").css("opacity","1");
+  };
+
+  setTimeout(textAppear, 2000);
+  setTimeout(rotate, 500);
+  setTimeout(moveToPositions, 800);
 
 });
 
