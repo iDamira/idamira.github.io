a8c5d94466d60573d859b420d49ea23a07a90297
Damira Ibragimova <idamira@Damiras-MBP.home>
2017-05-10 11:40:33 -0400

    added links, images, improved some animations

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
   console.log("js connected");
-
+// GLOBAL VARIABLES
   var bracketLeft = $("#bracket-left");
   var bracketRight = $("#bracket-right");
+  var footer = $("#footer-block");
+  var header = $("#header-block");
+  var body = $("#body-block");
+  var logo = $("h1, p");
+// GLOBAL VARIABLES
+
+// FUNCTIONS
+
+  var changeLogoColor = function(){
+    logo.css('color','grey');
+  };
 
   var rotate = function(){
     bracketLeft.css({
@@ -10,19 +21,17 @@ $(document).ready(function(){
       '-webkit-transform': 'rotate(-360deg)',
       '-o-transform': 'rotate(-360deg)',
       '-ms-transform': 'rotate(-360deg)',
-      'transform': 'rotate(-360deg)'
-    }).animate({
-      opacity: 1
-    }, 400);
+      'transform': 'rotate(-360deg)',
+      'opacity': 1
+    });
     bracketRight.css({
       '-moz-transform': 'rotate(360deg)',
       '-webkit-transform': 'rotate(360deg)',
       '-o-transform': 'rotate(360deg)',
       '-ms-transform': 'rotate(360deg)',
-      'transform': 'rotate(360deg)'
-    }).animate({
-      opacity: 1
-    }, 400);
+      'transform': 'rotate(360deg)',
+      'opacity': 1
+    });
   };
 
   var moveToPositions = function(){
@@ -38,11 +47,28 @@ $(document).ready(function(){
     $("h1").css("opacity","1");
   };
 
+  var removeBlocks = function(){
+    footer.animate({
+      right: '100vw'
+    }, 2000);
+    header.animate({
+      right: '100vw'
+    }, 2000);
+    body.animate({
+      left: '100vw'
+    }, 2000);
+  };
+// FUNCTIONS
+
+// FUNCTIONS INVOCATIONS
   setTimeout(rotate, 1000);
   setTimeout(moveToPositions, 1300);
   setTimeout(textAppear, 2500);
+  setTimeout(removeBlocks, 3500);
+  setTimeout(changeLogoColor, 4500);
+// FUNCTIONS INVOCATIONS
 
+}); //END OF DOCUMENT.ready
 
-});
 
 
